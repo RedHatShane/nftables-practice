aa87ba37c2389dcfc6e4a126c2cc5a0470146bc9
Shane <35788056+RedHatShane@users.noreply.github.com>
2024-11-01 00:15:15 -0700

    Update main.tf

--- extras/main.tf
+++ extras/main.tf
@@ -1,12 +1,15 @@
-# Create new lab Droplet (lab-a) in SF03 region
 
+# These are defined in terraform.tfvars
 variable "default" {}
 variable "droplets" {}
 
+# SSH key ID determined from key name (terraform.tfvars)
 data "digitalocean_ssh_key" "key" {
   name = var.default.ssh_key_name
 }
 
+# For each defined droplet, create a droplet
+# Pull user data (cloud config) from 'cloud-init' file
 resource "digitalocean_droplet" "droplets" {
   for_each = var.droplets
   image = var.default.image
@@ -17,10 +20,12 @@ resource "digitalocean_droplet" "droplets" {
   user_data = file("${path.module}/cloud-init")
 }
 
+# Collect information about my domain
 data "digitalocean_domain" "default" {
   name = "4cm3.lol"
 }
 
+# Add an A record for each droplet (matching hostname)
 resource "digitalocean_record" "record" {
   for_each = digitalocean_droplet.droplets
   domain = data.digitalocean_domain.default.id
